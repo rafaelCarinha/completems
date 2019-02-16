957ae50f820187513456a8663ec13659c6f72b9b
rafaelCarinha <rafael.marins@gorillalogic.com>
2019-02-16 14:45:12 -0600

    Initial Commit

---
+++ src/main/java/com/completems/CompletemsApplication.java
@@ -0,0 +1,14 @@
+package com.completems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompletemsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompletemsApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/completems/CompletemsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.completems;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CompletemsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
