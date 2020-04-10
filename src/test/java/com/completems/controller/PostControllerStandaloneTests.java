3d280972b98ec816ec3b3fd194acc30052560324
rafaelCarinha <rcarinha@hotmail.com>
2020-04-10 16:42:12 -0600

    Upgrading SpringBoot and SpringCloud version. Fixing Unit tests.

--- src/test/java/com/completems/controller/PostControllerStandaloneTests.java
+++ src/test/java/com/completems/controller/PostControllerStandaloneTests.java
@@ -13,11 +13,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PostController.class)
@@ -37,7 +38,7 @@ public class PostControllerStandaloneTests {
         Mockito.when(postService.findAll()).thenReturn(posts);
 
         mockMvc.perform(MockMvcRequestBuilders
-                .get("/blog/api/v1/listAll"))
+                .get("/api/v1/post/listAll"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", Matchers.hasSize(1)))
                 .andExpect(jsonPath("$[0].tittle", Matchers.is("Tittle")));
